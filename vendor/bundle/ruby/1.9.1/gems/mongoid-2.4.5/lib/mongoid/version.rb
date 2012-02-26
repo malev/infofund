d4a841d00e6f33d4dce7db957f578302e36a0387
Marcos Vanetta <marcosvanetta@gmail.com>
2012-02-26 19:49:31 -0300

    removed vendor

--- vendor/bundle/ruby/1.9.1/gems/mongoid-2.4.5/lib/mongoid/version.rb
+++
@@ -1,4 +0,0 @@
-# encoding: utf-8
-module Mongoid #:nodoc
-  VERSION = "2.4.5"
-end
--- vendor/bundle/ruby/1.9.1/gems/padrino-0.10.6.c/lib/padrino.rb
+++
@@ -1,6 +0,0 @@
-### AUTOMATICALLY GENERATED. DO NOT EDIT!
-require 'padrino-core'
-require 'padrino-mailer'
-require 'padrino-helpers'
-require 'padrino-admin'
-require 'padrino-cache'
