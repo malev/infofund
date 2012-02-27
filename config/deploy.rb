ede9dbc91cbbcdc3ed77d4a15f000ac101ea1d2e
Marcos Vanetta <marcosvanetta@gmail.com>
2012-02-27 17:15:05 -0300

    improving deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,33 @@
 require 'bundler/capistrano'
-#require "whenever/capistrano"
+require "whenever/capistrano"
 
 set :application, "infofund"
-set :rvm_ruby_string, '1.9.2@infofund'
 
+set :user, "malev"
+set :rvm_ruby_string, '1.9.2@infofund'
 set :domain, "www.villabom.com.ar"
 set :environment, "production"
-set :branch, "master"
-set :deploy_to, "/home/malev/sites/infofund"
-
-set :normalize_asset_timestamps, false
+set :deploy_to, "/home/malev/sites/#{application}"
 
 role :app, domain
 role :web, domain
 role :db, domain, :primary => true
 
-default_run_options[:pty] = true
-
-default_run_options[:shell] = 'bash'
-
-default_environment["RAILS_ENV"] = 'production'
-
-set :repository, "git://github.com/malev/infofund.git"
-set :deploy_via, :remote_cache
+set :normalize_asset_timestamps, false
 
 set :scm, :git
+set :repository, "git://github.com/malev/infofund.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 set :scm_verbose, true
 set :use_sudo, false
 set :ssh_options, :forward_agent => true
 
-set :user, "malev"
 set :keep_releases, 5
 
 namespace :deploy do
   desc "symlink to mongoid.yml"
-  task :create_symlink do
+  task :create_symlink_shared do
     run "ln -nfs #{deploy_to}/shared/config/mongoid.yml #{release_path}/config/mongoid.yml"
   end
 
@@ -48,4 +41,4 @@ namespace :deploy do
   end
 end
 
-after 'deploy:update_code', 'deploy:create_symlink', 'deploy:restart'
+after 'deploy:update_code', 'deploy:create_symlink_shared'
