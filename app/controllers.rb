42084c67b0671995483a9597dd84c8c3b6722f17
Marcos Vanetta <marcos.vanetta@threepillarglobal.com>
2012-03-06 18:22:25 -0300

    login pages

--- app/controllers.rb
+++ app/controllers.rb
@@ -20,6 +20,10 @@ Infofund.controllers  do
     @location = "about"
   end
 
+  get '/login' do
+    render 'login'
+  end
+
   get :fund, :with => :id, :provides => :json do
     @fund = Fund.find(params[:id])
 
@@ -28,24 +32,4 @@ Infofund.controllers  do
       :values => @fund.daily_value_variation
     }.to_json
   end
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
-
-  
 end
